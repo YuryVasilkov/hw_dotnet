fa9b9050a684768e2acc2de642631ac00ba5cbf1
Yury Vasilkov <newbid@yandex.ru>
2021-10-12 19:12:22 +0300

    HW_03:Add to GIT

---
+++ HomeWork_03/HW_03_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW_03_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            int addNumber(string number)
+            {
+                Console.WriteLine("Please, input {0} number", number);
+                string num1 = Console.ReadLine();
+                while (!int.TryParse(num1, out int num)) 
+                {
+                    Console.WriteLine("Incorrect data. Please, try again");
+                    num1 = Console.ReadLine();                    
+                }              
+               
+                return Convert.ToInt32(num1);
+            }
+            
+            Console.WriteLine("First number + Second number = {0}", addNumber("first") + addNumber("second"));
+
+
+        }
+    }
+}
---
+++ HomeWork_03/HW_03_02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HW_03_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int addNumber(string number)
+            {
+                Console.WriteLine("Please, input {0} number", number);
+                string num1 = Console.ReadLine();
+                while (!int.TryParse(num1, out int num))
+                {
+                    Console.WriteLine("Incorrect data. Please, try again");
+                    num1 = Console.ReadLine();
+                }
+
+                return Convert.ToInt32(num1);
+            }            
+
+            int result = addNumber("first") + addNumber("second");
+            Console.WriteLine("What will be the result?");
+            int intentedResult;
+            while (true)
+            {
+                intentedResult = addNumber("intented");
+                if (result == intentedResult)
+                {
+                    Console.WriteLine("Correct! :)");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect :(. Please, try again.");
+                }
+            }
+        }
+    }
+}
