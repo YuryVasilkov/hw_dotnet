6092151b6bd3fafd07bc200a5e2ba93d3cc66ffd
Yury Vasilkov <newbid@yandex.ru>
2021-10-14 21:06:01 +0300

    HW_05: Complete 3rd task.

---
+++ Homeworks_05/HW_05_03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HW_05_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void PrintArray(int[] array)
+            {
+                foreach (var item in array)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int temp;
+
+            Console.WriteLine("Source array:");
+            PrintArray(numbers);
+
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                temp = numbers[i];
+                numbers[i] = numbers[numbers.Length - 1 - i];
+                numbers[numbers.Length - 1 - i] = temp;
+            }
+
+            Console.WriteLine("The resulting array");
+            PrintArray(numbers);
+        }
+    }
+}
