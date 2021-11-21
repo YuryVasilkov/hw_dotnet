bf75ccd14adcdc796515153a4616799a1762bff7
Yury Vasilkov <newbid@yandex.ru>
2021-11-21 17:30:42 +0300

    HW_13_02: Complete 2nd task.

---
+++ Homeworks_13/HW_13_02/Motorcycle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW_13_02
+{
+    class Motorcycle
+    {
+        public Guid Guid { get; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Odometer { get; set; }
+
+        public Motorcycle()
+        {
+
+        }
+        public Motorcycle(int id, string name, string model, int year, int odometer)
+        {
+            Guid = Guid.NewGuid();
+            ID = id;
+            Name = name;
+            Model = model;
+            Year = year;
+            Odometer = odometer;
+        }
+    }
+}
--- Homeworks_13/HW_13_02/Program.cs
+++ Homeworks_13/HW_13_02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Serilog;
 
 namespace HW_13_02
 {
@@ -6,7 +7,41 @@ namespace HW_13_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration().WriteTo.File(@"C:\Temp\Log.log", rollingInterval: RollingInterval.Hour)
+                                                  .WriteTo.Console().CreateLogger();
+
+            Log.Warning("Application Homeworks_13.HW_13.02 was created!");
+
+            MotoArray array = new();
+            MotoList list = new();
+            Exception MotorcycleNotFoundException = new(message: "Motorcycle not found!");
+            Log.Information($"{array}, {list}, {MotorcycleNotFoundException} were created.");
+
+            array.UpdateMotorcycleByID(3);
+            Log.Information($"{array} was updated!");
+            Console.Clear();
+            Log.Information("Console was cleared.");
+            array.GetAllMotorcycles();
+
+            Console.WriteLine(new string('*', 60));
+
+            list.GetMotorcycleByID(4);
+            list.CreateMotorcycle(new Motorcycle(6, "Honda", "ViperSport", 2015, 5000));
+            list.GetAllMotorcycles();
+
+            try
+            {
+                throw MotorcycleNotFoundException;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error!");
+            }
+            finally
+            {
+                Log.Warning("Finish work!");
+                Log.CloseAndFlush();
+            }
         }
     }
 }
