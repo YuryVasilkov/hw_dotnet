00aa0247129a3ab55eee56e52aa1ecef4cf09c14
Yury Vasilkov <newbid@yandex.ru>
2021-12-14 13:59:47 +0300

    HW_18: using LINQ in Practice 07.Task03

---
+++ Homework_18/07.Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomNumbers = new int[133];
+            Random generator = new();
+
+            Console.WriteLine("Current array:");
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                randomNumbers[i] = generator.Next(1, 100);
+                Console.Write(randomNumbers[i] + " ");
+            }
+
+            Console.WriteLine();
+            var maxElement = randomNumbers.Max();
+            int index = Array.IndexOf(randomNumbers, maxElement);
+
+            Console.WriteLine($"Max element is {maxElement}. Its position is {index}");
+
+
+        }
+    }
+}
